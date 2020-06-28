c39905b7402c9ee96514713a49a72687138ca19e
RedStoneMatt <13626901+RedStoneMatt@users.noreply.github.com>
2020-06-28 20:52:13 +0200

    Added French Translation
    -Translated public/src/js/strings.js
    -Added French Entry to app.py
    -Added accented letters to the TnT.ttf font

--- app.py
+++ app.py
@@ -229,7 +229,7 @@ def route_admin_songs_new_post():
     output['enabled'] = True if request.form.get('enabled') else False
     output['title'] = request.form.get('title') or None
     output['subtitle'] = request.form.get('subtitle') or None
-    for lang in ['ja', 'en', 'cn', 'tw', 'ko']:
+    for lang in ['ja', 'en', 'cn', 'tw', 'ko', 'fr']:
         output['title_lang'][lang] = request.form.get('title_%s' % lang) or None
         output['subtitle_lang'][lang] = request.form.get('subtitle_%s' % lang) or None
 
@@ -289,7 +289,7 @@ def route_admin_songs_id_post(id):
 
     output['title'] = request.form.get('title') or None
     output['subtitle'] = request.form.get('subtitle') or None
-    for lang in ['ja', 'en', 'cn', 'tw', 'ko']:
+    for lang in ['ja', 'en', 'cn', 'tw', 'ko', 'fr']:
         output['title_lang'][lang] = request.form.get('title_%s' % lang) or None
         output['subtitle_lang'][lang] = request.form.get('subtitle_%s' % lang) or None
 
